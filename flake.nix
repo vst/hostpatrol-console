258121eb1d8f4f6f032e6aa6c79fd49062b9a561
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2026-07-24 09:10:52 +0800

    chore(deps): upgrade nixpkgs to v26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Host Patrol Console Application";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
